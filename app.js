0c3b548a6510f580ad16a73c3491f8c0f8e2204d
Ryan Snodgrass <rsnodgra92@gmail.com>
2021-04-29 11:38:48 -0600

    articles endpoints

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const articlesRouter = require('./routes/articles')
 
 const mongoose = require('mongoose');
 
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/articles',articlesRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
